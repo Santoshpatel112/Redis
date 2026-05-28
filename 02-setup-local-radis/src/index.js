05d755528887e73dbc62f8f971261e44c0a7bc50
Santosh Patel <santoshpatelvns5@gmail.com>
2026-05-28 14:28:07 +0530

    Redis codebase

---
+++ 02-setup-local-radis/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import Redis from "ioredis";
+const app=express();
+
+export const redis=new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.get("/redis",async(req,res)=>{
+    const reply=await redis.ping();
+    return res.json({message:"Hello from Express and Redis!", reply});
+})
+app.get("/mongo",async(req,res)=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/radis");
+        return res.json({
+            message:"Hello from Express and MongoDB!",
+            database: mongoose.connection.db.databaseName
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        return res.status(500).json({error:"Failed to connect to MongoDB"});
+    }
+})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
